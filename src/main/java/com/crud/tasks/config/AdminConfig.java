9207d947d673790b36613fc106d96cf09566698e
Wojciech Kurcab <rr151459@walstead.pl>
2019-11-22 21:47:49 +0100

    zadanie 32.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -11,4 +11,17 @@ public class AdminConfig {
     private String adminMail;
     @Value("${cc.mail}")
     private String ccMail;
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+    @Value("${info.company.goal}")
+    private String companyGoal;
+    @Value("${info.company.phone}")
+    private String companyPhone;
+    @Value("${info.app.administrator.address.street}")
+    private String companyAddressStreet;
+    @Value("${info.app.administrator.address.number}")
+    private String companyAddressNumber;
 }
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,25 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("companyName", adminConfig.getCompanyName());
+        context.setVariable("companyGoal", adminConfig.getCompanyGoal());
+        context.setVariable("companyAddress",
+                adminConfig.getCompanyAddressStreet() + " " +
+                        adminConfig.getCompanyAddressNumber());
+        context.setVariable("companyPhone", "phone: " + adminConfig.getCompanyPhone());
+        context.setVariable("goodbye_message", "Best regards ;)");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
